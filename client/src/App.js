2920630492a4a515d222d4a4a1f494ede3c4d1f3
NikSubbo <nikolai.subbotin@gmail.com>
2021-02-23 23:01:25 +0300

    path fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import Main from 'pages/Main/Main';
+import { Route } from "react-router-dom";
 
 function App() {
   return (
-    <Main />
+    <Route path="/" component={Main} />
   );
 }
 
